a8ca057c2b7f7c060d1918f8557496c8cc41fe1c
Bogdan Solga <bogdan.solga@gmail.com>
2017-07-22 12:51:11 +0300

    [clean + improve] Removed the caching samples and the JDBCTemplate, added a product saving

--- d02/d02s05/d02s05e01-embedded-database/src/main/java/net/safedata/springboot/training/d02/s05/service/ProductService.java
+++ d02/d02s05/d02s05e01-embedded-database/src/main/java/net/safedata/springboot/training/d02/s05/service/ProductService.java
@@ -3,44 +3,47 @@ package net.safedata.springboot.training.d02.s05.service;
 import net.safedata.springboot.training.d02.s05.model.Product;
 import net.safedata.springboot.training.d02.s05.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.PostConstruct;
 
 @Service
 public class ProductService {
 
-    private final JdbcTemplate jdbcTemplate;
     private final ProductRepository productRepository;
 
     @Autowired
-    public ProductService(final JdbcTemplate jdbcTemplate, final ProductRepository productRepository) {
-        this.jdbcTemplate = jdbcTemplate; this.productRepository = productRepository;
+    public ProductService(final ProductRepository productRepository) {
+        this.productRepository = productRepository;
     }
 
-    @Scheduled(fixedDelay = 10000)
-    public void searchForProducts() {
-        //TODO insert complex logic here
+    @PostConstruct
+    public void bootstrap() {
+        final Product product = new Product();
+        product.setName("iSomething");
+        product.setPrice(250d);
 
-        jdbcTemplate.execute("SELECT * FROM product p WHERE p.name = ?");
+        create(product);
     }
 
+    @Transactional(
+            readOnly = false,
+            propagation = Propagation.REQUIRED,
+            rollbackFor = {
+                    IllegalArgumentException.class,
+                    IllegalAccessException.class
+            }
+    )
     public void create(final Product product) {
         productRepository.save(product);
     }
 
     @Transactional(
             readOnly = true,
-            propagation = Propagation.SUPPORTS,
-            isolation = Isolation.READ_COMMITTED,
-            rollbackFor = {IllegalArgumentException.class, IllegalAccessException.class}
+            propagation = Propagation.SUPPORTS
     )
-    @Cacheable(cacheManager = "cacheManager", cacheNames = "products", condition = "id > 10")
     public Product get(final int id) {
         return productRepository.findOne(id);
     }
@@ -57,7 +60,6 @@ public class ProductService {
         productRepository.save(product);
     }
 
-    @CacheEvict(cacheNames = "products", condition = "id > 10")
     public void delete(final int id) {
         productRepository.delete(id);
     }
