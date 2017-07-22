5734bc47d9df89ad0735c7965951b44756e5b6fc
Bogdan Solga <bogdan.solga@gmail.com>
2017-07-22 13:27:57 +0300

    [clean] Removed the redirect sample, use RestController i/o Controller

--- d02/d02s01/src/main/java/net/safedata/springboot/training/d03/s01/SimpleRestController.java
+++ d02/d02s01/src/main/java/net/safedata/springboot/training/d03/s01/SimpleRestController.java
@@ -1,25 +1,17 @@
 package net.safedata.springboot.training.d03.s01;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.time.LocalTime;
+
+@RestController
 public class SimpleRestController {
 
     @RequestMapping(
-            value = "/hello"
+            path = "/hello"
     )
-    public @ResponseBody String hello() {
-        return "Hello, Spring Boot!";
-    }
-
-    // a redirect sample - the method response is a page name, not a ResponseBody
-    @RequestMapping(
-            value = "/redirect/{pageName}"
-    )
-    public String redirect(@PathVariable final String pageName) {
-        return "redirect:" + pageName;
+    public String hello() {
+        return "Hello, Spring Boot! The current time is " + LocalTime.now();
     }
 }
