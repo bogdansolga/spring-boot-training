a6c3d228393b2593b48623030d5af048761ed50d
Bogdan Solga <bogdan.solga@gmail.com>
2017-07-22 13:25:48 +0300

    [improve] Added the Environment usage

--- d01/d01s05/d01s05e02-bean-profiles/src/main/java/net/safedata/springboot/training/d01/s05/service/ProductService.java
+++ d01/d01s05/d01s05e02-bean-profiles/src/main/java/net/safedata/springboot/training/d01/s05/service/ProductService.java
@@ -1,6 +1,12 @@
 package net.safedata.springboot.training.d01.s05.service;
 
+import net.safedata.springboot.training.d01.s05.config.Profiles;
 import net.safedata.springboot.training.d01.s05.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
 
 /**
  * A simple {@link ProductService}, which uses the {@link ProductRepository} as a manually wired collaborator
@@ -9,13 +15,26 @@ import net.safedata.springboot.training.d01.s05.repository.ProductRepository;
  */
 public class ProductService {
 
+    @Autowired
+    private Environment environment;
+
     private final ProductRepository productRepository;
 
     public ProductService(final ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
+    @PostConstruct
+    public void displayProfiles() {
+        System.out.println("Running with the '" + Arrays.asList(environment.getActiveProfiles()) + "' profiles...");
+        System.out.println("Default profiles: '" + Arrays.asList(environment.getDefaultProfiles()) + "'");
+    }
+
     public void displayProducts() {
+        if (environment.acceptsProfiles(Profiles.PROD)) {
+            System.out.println("Do something only when prod is enabled...");
+        }
+
         productRepository.displayProducts();
     }
 }
