60c9a1c7f9c32cc57a64b5e120d2c243b99fb706
Bogdan Solga <bogdan.solga@gmail.com>
2017-07-22 13:02:08 +0300

    [clean] Removed some unneeded excluded configurations

--- d02/d02s03/d02s03e04-static-content/src/main/java/net/safedata/springboot/training/d02/s03/StaticContentServingDemo.java
+++ d02/d02s03/d02s03e04-static-content/src/main/java/net/safedata/springboot/training/d02/s03/StaticContentServingDemo.java
@@ -1,20 +1,14 @@
 package net.safedata.springboot.training.d02.s03;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.PropertyPlaceholderAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.MultipartAutoConfiguration;
 
 /**
  * A small Spring Boot demo used to showcase serving static content from the embedded Tomcat
  *
  * @author bogdan.solga
  */
-@SpringBootApplication(exclude = {
-        MultipartAutoConfiguration.class,
-        JmxAutoConfiguration.class
-})
+@SpringBootApplication
 public class StaticContentServingDemo {
 
     public static void main(String[] args) {
