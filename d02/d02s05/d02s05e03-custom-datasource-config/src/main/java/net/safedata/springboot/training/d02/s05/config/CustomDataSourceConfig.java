92bbc423217a07a66f813b4377598e37244d1e89
Bogdan Solga <bogdan.solga@gmail.com>
2017-07-27 17:00:33 +0300

    [improve] Use the config options via Value, created a Tomcat connection pool

--- d02/d02s05/d02s05e03-custom-datasource-config/src/main/java/net/safedata/springboot/training/d02/s05/config/CustomDataSourceConfig.java
+++ d02/d02s05/d02s05e03-custom-datasource-config/src/main/java/net/safedata/springboot/training/d02/s05/config/CustomDataSourceConfig.java
@@ -2,21 +2,19 @@ package net.safedata.springboot.training.d02.s05.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.bind.RelaxedPropertyResolver;
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
 
 /**
  * A simple {@link javax.sql.DataSource} configuration, which:
  * <ul>
  *     <li>configures the JPA repositories, using the {@link EnableJpaRepositories} annotation</li>
- *     <li>configures a custom {@link DataSource}, using the {@link HikariDataSource} class</li>
+ *     <li>configures a custom {@link javax.sql.DataSource}, using the {@link HikariDataSource} class</li>
  * </ul>
  *
  * @author bogdan.solga
@@ -28,26 +26,50 @@ public class CustomDataSourceConfig {
 
     private static final int AVAILABLE_PROCESSORS = Runtime.getRuntime().availableProcessors();
 
-    // as properties from YAML files cannot be wired using @Value, we need to use a RelaxedPropertyResolver
-    // to read the config values
-    private final RelaxedPropertyResolver propertyResolver;
+    @Value("${spring.datasource.url}")
+    private String url;
 
-    @Autowired
-    public CustomDataSourceConfig(final Environment environment) {
-        propertyResolver = new RelaxedPropertyResolver(environment, "spring.datasource.");
-    }
+    @Value("${spring.datasource.username}")
+    private String userName;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
 
     // if the @Bean is commented, the configured data-source will be wired
+    @Primary
     @Bean
-    public DataSource dataSource() {
+    public javax.sql.DataSource hikariConnectionPool() {
         final HikariConfig hikariConfig = new HikariConfig();
 
-        hikariConfig.setPoolName("spring-boot-demo-pool");
+        hikariConfig.setPoolName("hikari-connection-pool");
         hikariConfig.setMaximumPoolSize(AVAILABLE_PROCESSORS * 2);
-        hikariConfig.setJdbcUrl(propertyResolver.getProperty("url"));
-        hikariConfig.setUsername(propertyResolver.getProperty("username"));
-        hikariConfig.setPassword(propertyResolver.getProperty("password"));
+        hikariConfig.setConnectionTimeout(30000);
+        hikariConfig.setIdleTimeout(60000);
+        hikariConfig.setMaxLifetime(120000);
+        hikariConfig.setJdbcUrl(url);
+        hikariConfig.setUsername(userName);
+        hikariConfig.setPassword(password);
+        hikariConfig.setDriverClassName(driverClassName);
 
         return new HikariDataSource(hikariConfig);
+    }
+
+    @Bean
+    public javax.sql.DataSource tomcatConnectionPool() {
+        final DataSource dataSource = new DataSource();
+
+        dataSource.setName("tomcat-connection-pool");
+        dataSource.setUrl(url);
+        dataSource.setUsername(userName);
+        dataSource.setPassword(password);
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setMinIdle(2);
+        dataSource.setMaxIdle(AVAILABLE_PROCESSORS / 2);
+        dataSource.setMaxActive(AVAILABLE_PROCESSORS * 2 + 2);
+
+        return dataSource;
     }
 }
