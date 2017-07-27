a34a452151f0c78cde7a1c27a0f0a369f7c17ec6
Bogdan Solga <bogdan.solga@gmail.com>
2017-07-27 16:54:59 +0300

    [improve] Added a few auth usage samples

--- d03/d03s02/src/main/java/net/safedata/springboot/training/d03/s01/controller/ProductController.java
+++ d03/d03s02/src/main/java/net/safedata/springboot/training/d03/s01/controller/ProductController.java
@@ -1,11 +1,13 @@
 package net.safedata.springboot.training.d03.s01.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 import static net.safedata.springboot.training.d03.s01.config.Roles.*;
 
@@ -15,15 +17,24 @@ import static net.safedata.springboot.training.d03.s01.config.Roles.*;
 public class ProductController {
 
     @PreAuthorize("hasAnyRole('" + ADMIN_ROLE + "', '" + MANAGER_ROLE + "')")
-    public void addProduct() {
-
+    public void addProduct(final Authentication authentication) {
+        // further use the Authentication object, if needed
     }
 
     // dynamically retrieving the authenticated user details
     public void passAuthenticatedUser(@AuthenticationPrincipal UserDetails userDetails) {
-        final String username = userDetails.getUsername();
-
+        /* the same details can be obtained using:
         final SecurityContext securityContext = SecurityContextHolder.getContext();
         final UserDetails details = (UserDetails) securityContext.getAuthentication().getPrincipal();
+        */
+
+        final String username = userDetails.getUsername();
+        // the user details can be further passed to the services
+    }
+
+    // recommended to be used when the principal details need to be consumed by an external tool / API
+    @GetMapping("/currentUser")
+    public Principal principal(final Principal principal) {
+        return principal;
     }
 }
