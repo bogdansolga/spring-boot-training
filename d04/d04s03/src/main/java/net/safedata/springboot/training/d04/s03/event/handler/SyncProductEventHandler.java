8086095fc3173b1bf613cc3880ecc4699f154d92
Bogdan Solga <bogdan.solga@gmail.com>
2017-08-01 14:40:15 +0300

    [improve] Added the transactional samples

--- d04/d04s03/src/main/java/net/safedata/springboot/training/d04/s03/event/handler/SyncProductEventHandler.java
+++ d04/d04s03/src/main/java/net/safedata/springboot/training/d04/s03/event/handler/SyncProductEventHandler.java
@@ -4,26 +4,42 @@ import net.safedata.springboot.training.d04.s03.event.ProductRetrievedEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
+@SuppressWarnings("unused")
 public class SyncProductEventHandler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SyncProductEventHandler.class);
 
-    @Async
-    @EventListener
+    @EventListener(ProductRetrievedEvent.class)
     public void processProduct(final ProductRetrievedEvent productRetrievedEvent) {
         LOGGER.info("The product '{}' was read", productRetrievedEvent.getProductName());
     }
 
     @TransactionalEventListener(
+            classes = ProductRetrievedEvent.class,
             phase = TransactionPhase.AFTER_COMPLETION
     )
     public void processSavedProduct(ProductRetrievedEvent productRetrievedEvent) {
-        LOGGER.info("Executing the saved event...");
+        LOGGER.info("Processing after completion...");
+    }
+
+    @TransactionalEventListener(
+            classes = ProductRetrievedEvent.class,
+            phase = TransactionPhase.AFTER_ROLLBACK
+    )
+    public void processAfterRollback(ProductRetrievedEvent productRetrievedEvent) {
+        LOGGER.info("Processing after rollback...");
+    }
+
+    @TransactionalEventListener(
+            classes = ProductRetrievedEvent.class,
+            phase = TransactionPhase.AFTER_COMMIT
+    )
+    public void processAfterCommit(ProductRetrievedEvent productRetrievedEvent) {
+        LOGGER.info("Processing after commit...");
     }
 }
--- d04/d04s03/src/main/java/net/safedata/springboot/training/d04/s03/service/ProductService.java
+++ d04/d04s03/src/main/java/net/safedata/springboot/training/d04/s03/service/ProductService.java
@@ -5,6 +5,8 @@ import net.safedata.springboot.training.d04.s03.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ProductService {
@@ -12,12 +14,19 @@ public class ProductService {
     private final ApplicationEventPublisher applicationEventPublisher;
 
     @Autowired
-    public ProductService(ApplicationEventPublisher applicationEventPublisher) {
+    public ProductService(final ApplicationEventPublisher applicationEventPublisher) {
         this.applicationEventPublisher = applicationEventPublisher;
     }
 
+    @Transactional(
+            readOnly = true,
+            propagation = Propagation.SUPPORTS
+    )
     public Product get(int id) {
         applicationEventPublisher.publishEvent(new ProductRetrievedEvent("iSomething"));
-        return new Product(10, "iSomething");
+
+        //if (true) throw new IllegalArgumentException("Throwing from the publisher");
+
+        return new Product(id, "iSomething");
     }
 }
