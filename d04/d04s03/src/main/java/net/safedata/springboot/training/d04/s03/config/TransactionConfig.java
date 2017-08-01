648c73634c99d6c03fbab352efad0cbac475f916
Bogdan Solga <bogdan.solga@gmail.com>
2017-08-01 14:43:45 +0300

    [dev] Added the transactional config

---
+++ d04/d04s03/src/main/java/net/safedata/springboot/training/d04/s03/config/TransactionConfig.java
@@ -0,0 +1,9 @@
+package net.safedata.springboot.training.d04.s03.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class TransactionConfig {
+}
