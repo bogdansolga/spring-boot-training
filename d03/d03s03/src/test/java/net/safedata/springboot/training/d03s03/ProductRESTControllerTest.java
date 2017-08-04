0026bf46e89e7670f1c6c9f4b0e94619cf0fa054
Bogdan Solga <bogdan.solga@gmail.com>
2017-08-04 10:12:42 +0300

    [clean] Removed an unused import

--- d03/d03s03/src/test/java/net/safedata/springboot/training/d03s03/ProductRESTControllerTest.java
+++ d03/d03s03/src/test/java/net/safedata/springboot/training/d03s03/ProductRESTControllerTest.java
@@ -14,7 +14,6 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
