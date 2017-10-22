dda2d200e3a148f3f3fe9638ecd0e2c11733cfce
Bogdan Solga <bogdan.solga@gmail.com>
2017-10-22 18:15:03 +0300

    [clean] Removed an unused import

--- d04/d04s01/d04s01e04-async-exception-handling/src/main/java/net/safedata/springboot/training/d04/s01/config/TaskExecutorsConfig.java
+++ d04/d04s01/d04s01e04-async-exception-handling/src/main/java/net/safedata/springboot/training/d04/s01/config/TaskExecutorsConfig.java
@@ -2,7 +2,6 @@ package net.safedata.springboot.training.d04.s01.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
