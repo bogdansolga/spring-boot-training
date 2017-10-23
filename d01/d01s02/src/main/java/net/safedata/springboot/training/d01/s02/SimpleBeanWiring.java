76e0d9196a827678cc97d50b25e8f0015f75ee9e
Bogdan Solga <bogdan.solga@gmail.com>
2017-10-23 06:20:23 +0300

    [dev] Added an XML config sample, use a toggle to switch between annotations and XML

--- d01/d01s02/src/main/java/net/safedata/springboot/training/d01/s02/SimpleBeanWiring.java
+++ d01/d01s02/src/main/java/net/safedata/springboot/training/d01/s02/SimpleBeanWiring.java
@@ -4,6 +4,7 @@ import net.safedata.springboot.training.d01.s02.config.DomainConfig;
 import net.safedata.springboot.training.d01.s02.service.ProductService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * A simple demo for the usage of a Spring {@link org.springframework.context.annotation.Bean} which has a wired
@@ -13,11 +14,18 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  */
 public class SimpleBeanWiring {
 
+    private static final boolean USE_ANNOTATIONS_CONFIG = true;
+
     public static void main(String[] args) {
-        final ApplicationContext applicationContext = new AnnotationConfigApplicationContext(DomainConfig.class);
+        final ApplicationContext applicationContext = buildAppContext();
 
         // retrieving and using the ProductService; it will have the dependency wired (injected)
         final ProductService productService = applicationContext.getBean(ProductService.class);
         productService.displayProducts();
+    }
+
+    private static ApplicationContext buildAppContext() {
+        return USE_ANNOTATIONS_CONFIG ? new AnnotationConfigApplicationContext(DomainConfig.class) :
+                new ClassPathXmlApplicationContext("applicationContext.xml");
     }
 }
