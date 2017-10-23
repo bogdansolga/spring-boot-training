cee1825f6f02373324b31c4b95bd080d75a44650
Bogdan Solga <bogdan.solga@gmail.com>
2017-10-23 06:43:16 +0300

    [dev] Added a component scan sample via XML config

--- d01/d01s04/d01s04e01-component-scan/src/main/java/net/safedata/springboot/training/d01/s04/ComponentScanDemo.java
+++ d01/d01s04/d01s04e01-component-scan/src/main/java/net/safedata/springboot/training/d01/s04/ComponentScanDemo.java
@@ -4,6 +4,7 @@ import net.safedata.springboot.training.d01.s04.config.ComponentScanConfig;
 import net.safedata.springboot.training.d01.s04.service.ProductService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * A simple demo of a Spring project which wires the {@link org.springframework.context.annotation.Bean}s through
@@ -13,10 +14,17 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  */
 public class ComponentScanDemo {
 
+    private static final boolean USE_ANNOTATIONS_CONFIG = false;
+
     public static void main(String[] args) {
-        final ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ComponentScanConfig.class);
+        final ApplicationContext applicationContext = buildApplicationContext();
 
         final ProductService productService = applicationContext.getBean(ProductService.class);
         productService.displayProducts();
+    }
+
+    private static ApplicationContext buildApplicationContext() {
+        return USE_ANNOTATIONS_CONFIG ? new AnnotationConfigApplicationContext(ComponentScanConfig.class) :
+                new ClassPathXmlApplicationContext("applicationContext.xml");
     }
 }
