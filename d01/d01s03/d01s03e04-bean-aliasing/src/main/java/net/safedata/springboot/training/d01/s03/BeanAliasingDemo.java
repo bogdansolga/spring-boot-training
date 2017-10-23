1dc2db8d68e75ad06461722c1e37ec17628ce18b
Bogdan Solga <bogdan.solga@gmail.com>
2017-10-23 06:38:07 +0300

    [dev] Added a bean aliasing sample configured via XML

--- d01/d01s03/d01s03e04-bean-aliasing/src/main/java/net/safedata/springboot/training/d01/s03/BeanAliasingDemo.java
+++ d01/d01s03/d01s03e04-bean-aliasing/src/main/java/net/safedata/springboot/training/d01/s03/BeanAliasingDemo.java
@@ -4,6 +4,7 @@ import net.safedata.springboot.training.d01.s03.repository.ProductRepository;
 import net.safedata.springboot.training.d01.s03.config.BeanAliasingConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * A simple demo of a Spring project to demo the {@link org.springframework.context.annotation.Bean} aliasing
@@ -12,10 +13,17 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  */
 public class BeanAliasingDemo {
 
+    private static final boolean USE_ANNOTATIONS_CONFIG = true;
+
     public static void main(String[] args) {
-        final ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BeanAliasingConfig.class);
+        final ApplicationContext applicationContext = buildApplicationContext();
 
         final ProductRepository productRepository = applicationContext.getBean("prodRepo", ProductRepository.class);
         productRepository.displayProducts();
+    }
+
+    private static ApplicationContext buildApplicationContext() {
+        return USE_ANNOTATIONS_CONFIG ? new AnnotationConfigApplicationContext(BeanAliasingConfig.class) :
+                new ClassPathXmlApplicationContext("applicationContext.xml");
     }
 }
