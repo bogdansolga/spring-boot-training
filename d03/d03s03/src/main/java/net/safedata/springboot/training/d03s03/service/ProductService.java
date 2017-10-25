6f8995e6b978bfb10bc4fcbbe2e77abbc3048398
Bogdan Solga <bogdan.solga@gmail.com>
2017-10-25 21:05:49 +0300

    [dev] Add two products on init

--- d03/d03s03/src/main/java/net/safedata/springboot/training/d03s03/service/ProductService.java
+++ d03/d03s03/src/main/java/net/safedata/springboot/training/d03s03/service/ProductService.java
@@ -7,8 +7,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 public class ProductService {
@@ -22,7 +26,16 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
-    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
+    @PostConstruct
+    public void init() {
+        saveProduct(new Product(1, "Tablet"));
+        saveProduct(new Product(2, "Phone"));
+    }
+
+    @Transactional(
+            readOnly = true,
+            propagation = Propagation.SUPPORTS
+    )
     public Product getProduct(final int id) {
         if (id == THROWING_ID) {
             throw new IllegalArgumentException("There is no product with the ID " + THROWING_ID);
@@ -31,16 +44,32 @@ public class ProductService {
         return productRepository.findOne(id);
     }
 
-    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
+    @Transactional(
+            readOnly = true,
+            propagation = Propagation.SUPPORTS
+    )
     public List<Product> getProducts() {
         final List<Product> products = new ArrayList<>();
         productRepository.findAll().forEach(products::add);
         return products;
     }
 
-    @Transactional(readOnly = false, propagation = Propagation.REQUIRED)
+    @Transactional(
+            readOnly = false,
+            propagation = Propagation.REQUIRED
+    )
     public String saveProduct(final Product product) {
+        // a lot of processing goes in here, before actually saving the product :)
         productRepository.save(product);
         return "OK";
+    }
+
+    @Transactional(
+            readOnly = true,
+            propagation = Propagation.SUPPORTS
+    )
+    public List<Product> getAll() {
+        return StreamSupport.stream(productRepository.findAll().spliterator(), false)
+                            .collect(Collectors.toList());
     }
 }
