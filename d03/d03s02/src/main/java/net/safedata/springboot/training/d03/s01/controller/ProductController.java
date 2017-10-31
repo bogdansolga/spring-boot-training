e8058264818512418e6ed6d74db6846b96801fd2
Bogdan Solga <bogdan.solga@gmail.com>
2017-10-31 05:37:48 +0200

    [improve] Added a few showcase methods

--- d03/d03s02/src/main/java/net/safedata/springboot/training/d03/s01/controller/ProductController.java
+++ d03/d03s02/src/main/java/net/safedata/springboot/training/d03/s01/controller/ProductController.java
@@ -5,8 +5,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import net.safedata.springboot.training.d03.s01.Product;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.security.Principal;
 
 import static net.safedata.springboot.training.d03.s01.config.Roles.*;
@@ -20,9 +25,18 @@ public class ProductController {
     public void addProduct(final Authentication authentication) {
         // further use the Authentication object, if needed
     }
+    
+    @GetMapping(
+    		path = "/product/{id}"
+	)
+    public Product getProduct(@PathVariable final int id, final @AuthenticationPrincipal UserDetails userDetails) {
+        final String username = userDetails.getUsername();
+        System.out.println("The current user is '" + username + "'");
+        return new Product(20, "Tablet");
+    }
 
     // dynamically retrieving the authenticated user details
-    public void passAuthenticatedUser(@AuthenticationPrincipal UserDetails userDetails) {
+    public void passAuthenticatedUser(final @AuthenticationPrincipal UserDetails userDetails) {
         /* the same details can be obtained using:
         final SecurityContext securityContext = SecurityContextHolder.getContext();
         final UserDetails details = (UserDetails) securityContext.getAuthentication().getPrincipal();
@@ -30,6 +44,10 @@ public class ProductController {
 
         final String username = userDetails.getUsername();
         // the user details can be further passed to the services
+    }
+
+    public void processRequestOrResponseParameters(final HttpServletRequest request, final HttpServletResponse response) {
+        // get parameters from the HTTP request, set details in the response
     }
 
     // recommended to be used when the principal details need to be consumed by an external tool / API
