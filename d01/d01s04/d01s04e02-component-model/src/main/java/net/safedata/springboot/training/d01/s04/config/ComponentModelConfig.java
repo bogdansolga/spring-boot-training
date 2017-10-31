0fc4662c5770390267f732aa169edbab26e345b1
Bogdan Solga <bogdan.solga@gmail.com>
2017-10-31 05:32:39 +0200

    [dev] Added an autowire by type example

--- d01/d01s04/d01s04e02-component-model/src/main/java/net/safedata/springboot/training/d01/s04/config/ComponentModelConfig.java
+++ d01/d01s04/d01s04e02-component-model/src/main/java/net/safedata/springboot/training/d01/s04/config/ComponentModelConfig.java
@@ -1,7 +1,12 @@
 package net.safedata.springboot.training.d01.s04.config;
 
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import net.safedata.springboot.training.d01.s04.repository.ProductRepository;
+import net.safedata.springboot.training.d01.s04.service.ProductService;
 
 /**
  * A simple Spring configuration, which wires the beans automatically via {@link ComponentScan}ing
@@ -11,4 +16,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages = "net.safedata.springboot.training.d01.s04")
 public class ComponentModelConfig {
+	
+	@Bean(autowire = Autowire.BY_TYPE)
+	public ProductService anotherProductService() {
+		return new ProductService(new ProductRepository());
+	}
 }
