60d04e3c8ae92a47cf5d304f05702f39822e5adc
Bogdan Solga <bogdan.solga@gmail.com>
2018-11-23 09:28:46 +0200

    [improve] Improved a few mappings

--- d02/d02s03/d02s03e01-spring-web-annotations/src/main/java/net/safedata/springboot/training/d02/s03/RequestMappingController.java
+++ d02/d02s03/d02s03e01-spring-web-annotations/src/main/java/net/safedata/springboot/training/d02/s03/RequestMappingController.java
@@ -1,7 +1,9 @@
 package net.safedata.springboot.training.d02.s03;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Date;
 import java.util.Optional;
 
@@ -45,23 +48,40 @@ public class RequestMappingController {
 
     @RequestMapping(
             method = RequestMethod.GET,
-            path = "/pathVariable/{first}/{second}"
+            path = {
+                    "/pathVariable/{first}",
+                    "/pathVariable/{first}/{second}"
+            }
     )
     public String pathVariableIntro(@PathVariable String first,
-                                    @PathVariable(required = false) String second) {
-        return "The first path variable value is '" + first + "', the second is '" + second + "'";
+                                    @PathVariable(required = false) Optional<String> second) {
+        return "The first path variable value is '" + first + "', the second is '" + second.orElse("N/A") + "'";
     }
 
     @RequestMapping(
             method = RequestMethod.GET
     )
     @SuppressWarnings("unused")
-    public String requestAndResponseUsage(final HttpServletRequest request, final HttpServletResponse response) {
+    public String requestAndResponseUsage(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
         return "We can pass the HttpServletRequest and HttpServletResponse objects to any RequestMapping annotated method";
     }
 
-    @GetMapping("/simple")
+    @RequestMapping(
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_OCTET_STREAM_VALUE
+    )
+    public void streamSomething(final HttpServletResponse response) throws IOException {
+        response.getOutputStream().write(new byte[]{});
+        response.setHeader("Content-Disposition", "file-name");
+    }
+
+    @GetMapping("/simpleGETMapping")
     public String simpleGetMapping() {
         return "A simple GetMapping";
+    }
+
+    @PostMapping("/simplePOSTMapping")
+    public String simplePostMapping() {
+        return "A simple PostMapping";
     }
 }
