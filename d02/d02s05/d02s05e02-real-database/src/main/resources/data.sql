d2560fd2007c13a220a2f979fac1c80eb7d0fd9d
Bogdan Solga <bogdan.solga@gmail.com>
2018-12-04 14:04:27 +0200

    [clean] Removed the transaction usage

--- d02/d02s05/d02s05e02-real-database/src/main/resources/data.sql
+++ d02/d02s05/d02s05e02-real-database/src/main/resources/data.sql
@@ -1,9 +1,5 @@
 -- run these commands before starting the project, in order to setup the database and the connecting user
-BEGIN TRANSACTION;
-
 CREATE USER postgres WITH PASSWORD 'postgres';
 
 CREATE DATABASE spring_boot;
 GRANT ALL PRIVILEGES ON DATABASE spring_boot TO postgres;
-
-COMMIT;
