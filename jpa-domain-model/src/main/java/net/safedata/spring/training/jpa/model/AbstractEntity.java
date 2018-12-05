48e6bec9c85f6ad59d087f711e3ccbfa5fa14d0b
Bogdan Solga <bogdan.solga@gmail.com>
2018-12-05 14:24:51 +0200

    [dev] Integrated the domain model and JPA domain model classes

---
+++ jpa-domain-model/src/main/java/net/safedata/spring/training/jpa/model/AbstractEntity.java
@@ -0,0 +1,6 @@
+package net.safedata.spring.training.jpa.model;
+
+import java.io.Serializable;
+
+public abstract class AbstractEntity implements Serializable {
+}
---
+++ jpa-domain-model/src/main/java/net/safedata/spring/training/jpa/model/StoreManager.java
@@ -0,0 +1,22 @@
+package net.safedata.spring.training.jpa.model;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "StoreManager")
+@SuppressWarnings("unused")
+public class StoreManager extends AbstractEntity {
+
+    @EmbeddedId
+    private StoreManagerPK storeManagerPK;
+
+    public StoreManagerPK getStoreManagerPK() {
+        return storeManagerPK;
+    }
+
+    public void setStoreManagerPK(StoreManagerPK storeManagerPK) {
+        this.storeManagerPK = storeManagerPK;
+    }
+}
