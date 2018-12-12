414ee9e363a8ff7b2a28ec6759e275a80bbc9183
Bogdan Solga <bogdan.solga@gmail.com>
2018-12-12 05:54:27 +0200

    [dev] Simplified the test setup

--- d03/d03s03/src/test/java/net/safedata/springboot/training/d03s03/ProductRESTControllerTest.java
+++ d03/d03s03/src/test/java/net/safedata/springboot/training/d03s03/ProductRESTControllerTest.java
@@ -1,39 +1,24 @@
 package net.safedata.springboot.training.d03s03;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-import javax.annotation.PostConstruct;
 
 import static org.hamcrest.Matchers.hasSize;
 
-@ExtendWith(SpringExtension.class)
-@WebAppConfiguration
-@ContextConfiguration(classes = ProductServiceDemo.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class ProductRESTControllerTest {
 
     @Autowired
-    private WebApplicationContext webApplicationContext;
-
     private MockMvc mockMvc;
-
-    @PostConstruct
-    void setup () {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                                 .build();
-    }
 
     @Test
     void given_thereAreTabletsInTheDatabase_andANewTabletIsCreated_whenRetrievingTablets_thenTheirNumberIsCorrect()
