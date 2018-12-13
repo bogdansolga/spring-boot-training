e3b67ee0e502e8719f796455e9fcd454864bb7eb
Bogdan Solga <bogdan.solga@gmail.com>
2018-12-13 13:31:07 +0200

    [dev] Added an actuator writing operation

--- d03/d03s04-spring-boot-actuator/src/main/java/net/safedata/springboot/training/d03/s04/endpoint/MemoryEndpoint.java
+++ d03/d03s04-spring-boot-actuator/src/main/java/net/safedata/springboot/training/d03/s04/endpoint/MemoryEndpoint.java
@@ -1,17 +1,22 @@
 package net.safedata.springboot.training.d03.s04.endpoint;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.annotation.WriteOperation;
 import org.springframework.stereotype.Component;
 
 /**
- * A sample Spring Boot Actuator endpoint
+ * A sample Spring Boot Actuator custom endpoint
  *
  * @author bogdan.solga
  */
 @Component
-@Endpoint(id = "memory")
+@Endpoint(id = "custom")
 public class MemoryEndpoint {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MemoryEndpoint.class);
 
     @ReadOperation
     public MemoryInfo memoryInfo() {
@@ -19,5 +24,13 @@ public class MemoryEndpoint {
         final long freeMemory = runtime.freeMemory();
 
         return new MemoryInfo(runtime.totalMemory() - freeMemory, freeMemory);
+    }
+
+    @WriteOperation
+    public String setASystemProperty(final String name, final String value) {
+        LOGGER.info("Setting the property '{}' with the value '{}'", name, value);
+        System.setProperty(name, value);
+
+        return "All good";
     }
 }
