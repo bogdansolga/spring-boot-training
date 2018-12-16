2676007a9e5ec8d9945be2ccdf44b6ea3ed8bb22
Bogdan Solga <bogdan.solga@gmail.com>
2018-12-16 12:06:04 +0200

    [fix] Enabled the transaction management

--- d03/d03s03-unit-and-integration-testing/src/main/java/net/safedata/springboot/training/d03s03/config/ProductServiceConfig.java
+++ d03/d03s03-unit-and-integration-testing/src/main/java/net/safedata/springboot/training/d03s03/config/ProductServiceConfig.java
@@ -3,9 +3,11 @@ package net.safedata.springboot.training.d03s03.config;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "net.safedata.springboot.training.d03s03.repository")
+@EnableTransactionManagement
 @EntityScan(basePackages = "net.safedata.spring.training.jpa.model")
 public class ProductServiceConfig {
 }
