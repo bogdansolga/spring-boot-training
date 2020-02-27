cf8be5be7761eac55039f28c8c32a5c206d35e81
Bogdan Solga <bogdan.solga@gmail.com>
2020-02-27 15:10:30 +0200

    [fix] Fixed the content

--- d02/d02s03-spring-web/d02s03e04-static-content/src/main/java/net/safedata/springboot/training/d02/s03/StaticContentServingDemo.java
+++ d02/d02s03-spring-web/d02s03e04-static-content/src/main/java/net/safedata/springboot/training/d02/s03/StaticContentServingDemo.java
@@ -3,8 +3,6 @@ package net.safedata.springboot.training.d02.s03;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.UUID;
-
 /**
  * A small Spring Boot demo used to showcase serving static content from the embedded Tomcat
  *
@@ -14,8 +12,6 @@ import java.util.UUID;
 public class StaticContentServingDemo {
 
     public static void main(String[] args) {
-        //SpringApplication.run(StaticContentServingDemo.class, args);
-        final String string = UUID.randomUUID().toString();
-        System.out.println(string + ", " + string.length());
+        SpringApplication.run(StaticContentServingDemo.class, args);
     }
 }
