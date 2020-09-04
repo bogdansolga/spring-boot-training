e0f9425a02343c36d981728a0084e890d0a0a804
Bogdan Solga <bogdan.solga@gmail.com>
2020-09-04 14:53:41 +0300

    [improve] Made the event listener async

--- d04/d04s03-events-pub-sub/src/main/java/net/safedata/springboot/training/d04/s03/service/AppInitMonitorService.java
+++ d04/d04s03-events-pub-sub/src/main/java/net/safedata/springboot/training/d04/s03/service/AppInitMonitorService.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,6 +12,7 @@ public class AppInitMonitorService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AppInitMonitorService.class);
 
+    @Async
     @EventListener(ApplicationReadyEvent.class)
     public void onAppInit(final ApplicationReadyEvent applicationReadyEvent) {
         LOGGER.info("The app has successfully started - {}", applicationReadyEvent.getApplicationContext()
