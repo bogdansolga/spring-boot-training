d798c540c3e1d779e987c3c8f9fbd1e7ed24982b
Bogdan Solga <bogdan.solga@gmail.com>
2020-09-04 14:52:20 +0300

    [dev] Added an async config

---
+++ d04/d04s03-events-pub-sub/src/main/java/net/safedata/springboot/training/d04/s03/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package net.safedata.springboot.training.d04.s03.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
