30ea1d70ca53b0853ab003b65ec814df68dfa6bb
Bogdan Solga <bogdan.solga@gmail.com>
2021-05-13 13:50:54 +0300

    [dev] Added the AsyncConfig

---
+++ d02/d02s05-database-access/d02s05e01-embedded-database/src/main/java/net/safedata/springboot/training/d02/s05/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package net.safedata.springboot.training.d02.s05.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
