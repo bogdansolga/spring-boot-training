c9a08346830eace9bb90733cbaa3ebe4ccca7186
Bogdan Solga <bogdan.solga@gmail.com>
2021-10-22 14:21:22 +0300

    [improve] Updated the database connection properties

--- d02/d02s05-database-access/d02s05e02-real-database/src/main/resources/data.sql
+++ d02/d02s05-database-access/d02s05e02-real-database/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 -- run these commands before starting the project, in order to setup the database and the connecting user
-CREATE USER postgres WITH PASSWORD 'postgres';
+CREATE USER spring_boot_admin WITH PASSWORD 'spring_boot_admin';
 
 CREATE DATABASE spring_boot;
-GRANT ALL PRIVILEGES ON DATABASE spring_boot TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE spring_boot TO spring_boot_admin;
