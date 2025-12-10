da77fd5273bbdad6f6db36485b07a9a69881a9a1
Bogdan Solga <bogdan.solga@gmail.com>
2025-12-10 14:00:31 +0200

    [fix] Fixed the unit integration tests

--- d03/d03s03-unit-and-integration-testing/src/test/java/net/safedata/springboot/training/d03s03/integration/ProductControllerTestIT.java
+++ d03/d03s03-unit-and-integration-testing/src/test/java/net/safedata/springboot/training/d03s03/integration/ProductControllerTestIT.java
@@ -1,91 +1,81 @@
 package net.safedata.springboot.training.d03s03.integration;
 
-import io.restassured.RestAssured;
-import io.restassured.filter.log.LogDetail;
-import io.restassured.http.ContentType;
 import net.safedata.springboot.training.d03s03.ProductServiceDemo;
 import net.safedata.springboot.training.d03s03.Profiles;
 import net.safedata.springboot.training.d03s03.dto.ProductDTO;
 import net.safedata.springboot.training.d03s03.service.ProductService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
-import static io.restassured.RestAssured.given;
+import java.util.stream.Stream;
+
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(
-        classes = ProductServiceDemo.class,
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
-)
+@SpringBootTest(classes = ProductServiceDemo.class)
+@AutoConfigureMockMvc
 @ActiveProfiles(Profiles.IN_MEMORY)
-public class ProductControllerTestIT extends AbstractTransactionalTestNGSpringContextTests {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Transactional
+public class ProductControllerTestIT {
 
     private static final String PRODUCT_NAME = "The product with the ID 1";
 
-    @LocalServerPort
-    private int port;
+    @Autowired
+    private MockMvc mockMvc;
 
     @Autowired
     private ProductService productService;
 
-    @BeforeMethod
-    public void init() {
-        RestAssured.port = port;
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails(LogDetail.ALL);
-    }
-
-    @BeforeClass
+    @BeforeAll
     public void initializeProduct() {
         productService.save(new ProductDTO(1, PRODUCT_NAME));
     }
 
     @Test
-    public void givenTheContentTypeIsCorrect_WhenGettingAProduct_ThenTheStatusAndTheNameAreCorrect() {
-        given()
-                .accept(ContentType.JSON).
-        when()
-                .get("/product/{id}", 1).
-        then()
-                .statusCode(HttpStatus.OK.value())
-                .body("productName", is(PRODUCT_NAME));
+    public void givenTheContentTypeIsCorrect_WhenGettingAProduct_ThenTheStatusAndTheNameAreCorrect() throws Exception {
+        mockMvc.perform(get("/product/{id}", 1)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.productName", is(PRODUCT_NAME)));
     }
 
     @Test
-    public void givenTheContentTypeIsCorrect_WhenGettingAllProducts_ThenAllGood() {
-        given()
-                .accept(ContentType.JSON).
-        when()
-                .get("/product").
-        then()
-                .statusCode(HttpStatus.OK.value())
-                .body("size()", is(1))
-                .body("[0].productName", is(PRODUCT_NAME));
+    public void givenTheContentTypeIsCorrect_WhenGettingAllProducts_ThenAllGood() throws Exception {
+        mockMvc.perform(get("/product")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].productName", is(PRODUCT_NAME)));
     }
 
-    // a sample of using a dataProvider
-    @Test(dataProvider = "dataProvider")
-    public void givenTheContentTypeIsCorrect_WhenUsingADataProvider_ThenAllGood(final String productId, final int statusCode) {
-        given()
-                .accept(ContentType.JSON).
-        when()
-                .get("/product/{id}", productId).
-        then()
-                .statusCode(statusCode);
+    // a sample of using a parameterized test (JUnit 5 equivalent of TestNG's @DataProvider)
+    @ParameterizedTest
+    @MethodSource("dataProvider")
+    public void givenTheContentTypeIsCorrect_WhenUsingADataProvider_ThenAllGood(final String productId, final int statusCode) throws Exception {
+        mockMvc.perform(get("/product/{id}", productId)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is(statusCode));
     }
 
-    @DataProvider(name = "dataProvider", parallel = true)
-    public Object[][] dataProvider() {
-        return new Object[][]{
-                { "1", HttpStatus.OK.value()}, // productId and status code
-                {"13", HttpStatus.BAD_REQUEST.value()}
-        };
+    static Stream<Arguments> dataProvider() {
+        return Stream.of(
+                Arguments.of("1", 200),   // productId and status code
+                Arguments.of("13", 400)
+        );
     }
 }
